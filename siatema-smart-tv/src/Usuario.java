9f8281a667e87e825810fa704397d341787ab871
Dayvid-William <dayvid.william5@outlook.com>
2023-03-12 13:07:30 -0300

    smartTv criada

--- siatema-smart-tv/src/Usuario.java
+++ siatema-smart-tv/src/Usuario.java
@@ -4,8 +4,28 @@ public class Usuario {
         SmartTv smartTv = new SmartTv();
 
         System.out.println("TV Ligada ?" + smartTv.ligada);
-        System.out.println("Canal Atual ?" + smartTv.canal);
-        System.out.println("Volume Atual ?" + smartTv.volume);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
 
+        smartTv.ligar();
+        System.out.println("Novo StatusTV -> Ligada ? " + smartTv.ligada);
+        
+        smartTv.desligar();
+        System.out.println("Novo StatusTV -> Ligada ? " + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.aumentarCanal();
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Canal Atual : " + smartTv.canal);
+
+        smartTv.mudarCanal(6);
+        System.out.println("Canal Atual : " + smartTv.canal);
     }
 }
